39f387e804b1e4738813a4ae8fbacd65cec7c7c4
unknown <broughton.24@gmail.com>
2021-10-30 13:39:28 -0400

    Created slack event to eventbus stack

---
+++ lambda-fns/event-handler/index.ts
@@ -0,0 +1,61 @@
+import { EventBridge } from 'aws-sdk';
+import { verifyRequestSignature } from './signature'
+
+export async function handler(event: any): Promise<any> {
+    console.log(JSON.stringify(event, undefined, 2));
+
+    const response = {
+        statusCode: 200,
+        body: '',
+    }
+    try {
+        if (!process.env.SLACK_SIGNING_SECRET) throw new Error('The environment variable SLACK_SIGNING_SECRET is not defined');
+
+        if (!event.body) throw new Error('Missing body');
+
+        if (!event.headers['X-Slack-Signature']) throw new Error('Missing X-Slack-Signature');
+
+        if (!event.headers['X-Slack-Request-Timestamp']) throw new Error('Missing X-Slack-Request-Timestamp');
+
+        if (!verifyRequestSignature({
+            body: event.body,
+            requestSignature: event.headers['X-Slack-Signature'],
+            requestTimestamp: parseInt(event.headers['X-Slack-Request-Timestamp'], 10),
+            signingSecret: process.env.SLACK_SIGNING_SECRET,
+        })) {
+            response.statusCode = 403;
+            return response;
+        }
+
+        const body = JSON.parse(event.body);
+        console.log('Body: %j', body);
+
+        if (body.type === 'url_verification') { // Slack URL verification, respond with challenge
+            console.log('URL verification');
+            response.body = JSON.stringify({ challenge: body.challenge });
+            return response;
+        }
+
+        const eventBridge = new EventBridge({ apiVersion: '2015-10-07' });
+
+        const putEvents = await eventBridge.putEvents({
+            Entries: [
+                {
+                    Detail: event.body,
+                    DetailType: 'Slack Event',
+                    Source: 'slack',
+                    Resources: [body.api_app_id],
+                    Time: new Date(body.event_time),
+                },
+            ],
+        }).promise();
+        console.log('Put events: %j', putEvents);
+
+        return response;
+    } catch (err) {
+        console.log(err);
+        response.statusCode = 500;
+        return response;
+    }
+}
+
--- lib/slack-weather-service-stack.ts
+++ lib/slack-weather-service-stack.ts
@@ -1,9 +1,38 @@
 import * as cdk from '@aws-cdk/core';
+import * as apigateway from '@aws-cdk/aws-apigatewayv2'
+import * as integrations from '@aws-cdk/aws-apigatewayv2-integrations'
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as logs from '@aws-cdk/aws-logs';
+import * as events from '@aws-cdk/aws-events'
+import * as path from 'path';
+import { EventBus } from '@aws-cdk/aws-events';
 
 export class SlackWeatherServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const handler = new NodejsFunction(this, 'handler', {
+      entry: path.join(__dirname, '..', 'lambda-fns', 'event-handler', 'index.ts'),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      logRetention: logs.RetentionDays.ONE_WEEK,
+      environment: {
+        SLACK_SIGNING_SECRET: '',
+      }
+    })
+
+    const httpApi = new apigateway.HttpApi(this, 'SlackApi', {
+      defaultIntegration: new integrations.LambdaProxyIntegration({
+        handler,
+      }
+      )
+    })
+
+    events.EventBus.grantAllPutEvents(handler);
+    
+    new cdk.CfnOutput(this, "ApiEndpoint", {
+      value: httpApi.apiEndpoint,
+      exportName: "ApiEndpoint"
+    })
   }
 }
